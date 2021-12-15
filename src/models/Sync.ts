1992ae6279c5dd0ab00deaba51e98c77c53d6d6f
Nam Pham <nam.pham@team.enouvo.com>
2021-12-15 23:36:38 +0700

    more on application

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosPromise } from "axios";
 
 interface HasId {
-    id: number;
+    id?: number;
 }
 
 export class Sync<T extends HasId> {
@@ -14,7 +14,7 @@ export class Sync<T extends HasId> {
     save(data: T): AxiosPromise {
         const { id } = data;
 
-        if ('id') {
+        if (id) {
             //put
             return axios.put(`${this.rootUrl}/${id}`, data);
         } else {
