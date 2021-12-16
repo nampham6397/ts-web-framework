d63c7b02d93a7057d19bffb4ff351db9f699348c
nampham <nam.pham@team.enouvo.com>
2021-12-16 18:27:21 +0700

    project completed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import { User } from './models/User';
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { UserProps, User } from "./models/User";
 
-const user = new User({ name: 'new record', age: 0 });
-
-user.on('change', () => {
-   console.log('user was changed');
+const users = new Collection(' http://localhost:3000/users', (json: UserProps) => {
+   return User.buildUser(json);
 });
+
+users.on('change', () => {
+    const root = document.getElementById('root');
+
+    if (root) {
+        new UserList(root, users).render();
+    }
+});
+
+users.fetch();
